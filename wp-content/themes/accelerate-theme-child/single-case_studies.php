9cfa3f389f2ee45b6b657989c7e5b83a988ba6df
benedettoadrianne <adriannescaleradesign@gmail.com>
2017-07-26 22:22:11 -0400

    began styling case study post

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,10 +14,9 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-
-      <h1> This is my first post</h1>
-
-				<?php the_content(); ?>
+				<article class="case-study">
+						<?php the_content(); ?>
+				</article>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
