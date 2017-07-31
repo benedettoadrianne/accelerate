7bc1581c71d7d7e2ccca88d902c3ff93dd0cfdaf
benedettoadrianne <adriannescaleradesign@gmail.com>
2017-07-31 14:26:15 -0400

    added archive page and fixed images in single-case_studies page

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -22,34 +22,27 @@ get_header(); ?>
 				$image_2 = get_field('image_2');
 				$image_3 = get_field('image_3');
 				$size = "full";?>
-
-				<article class="case-study">
+		<article class="case-study">
 						<aside class="case-study-sidebar">
 								<h2><?php the_title(); ?></h2>
 								<h5><?php echo $services; ?></h5>
 								<h6>Client: <?php echo $client; ?></h6>
-
 								<?php the_content(); ?>
-
 								<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
 						</aside>
-
 						<div class="case-study-images">
-								<?php if($image_1) { ?>
-										<echo wp_get_attachment_image( $image_1, $size );/>
-								<?php } ?>
-
-								<?php if($image_2) { ?>
-										<echo wp_get_attachment_image( $image_2, $size );/>
-								<?php } ?>
-
-								<?php if($image_3) { ?>
-										<echo wp_get_attachment_image( $image_3, $size );/>
-								<?php } ?>
+								<?php if($image_1) {
+										echo wp_get_attachment_image( $image_1, $size );
+								} ?>
+								<?php if($image_2) {
+										echo wp_get_attachment_image( $image_2, $size );
+							 	} ?>
+								<?php if($image_3) {
+										echo wp_get_attachment_image( $image_3, $size );
+								} ?>
 					</div>
-
-				</article>
-			<?php endwhile; // end of the loop. ?>
+			</article>
+	<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
