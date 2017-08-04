056cb0cc0713145fbedea0ee85f8b8cb795e17de
benedettoadrianne <adriannescaleradesign@gmail.com>
2017-08-04 13:43:45 -0400

    adding files to staging

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,7 +25,7 @@ get_header(); ?>
 		<div class="homepage-hero">
 				<div class="site-content">
 					<?php the_content(); ?>
-					<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
+					<a class="button" href="<?php echo home_url(); ?>/case-studies">View Our Work</a>
 				</div><!-- .site-content -->
 			<?php endwhile; // end of the loop. ?>
 		</div>
